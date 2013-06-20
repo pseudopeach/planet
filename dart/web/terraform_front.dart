0713d453a04b0ae6d37ce2f7015170edd5ed1ad9
Justin Armstrong <pseudopeach@gmail.com>
2013-06-20 13:07:58 -0400

    starts new css-based architecture
    * creates css file and basic html template
    * removes dart experiments

--- dart/web/terraform_front.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:html';
-import 'package:web_ui/web_ui.dart';
-
-void main() {
-  // Enable this to use Shadow DOM in the browser.
-  //useShadowDom = true;
-}
--- html/terraform-front/web/zion_cell.dart
+++
@@ -1,5 +0,0 @@
-class ZionCell {
-  ZionCell() {
-
-  }
-}
